6926a11675a6d510e6a44dcddf45a15d016d4a8a
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2025-12-01 13:33:48 +0100

    components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center p-8">
-    <h1 class="text-4xl font-bold mb-6 text-blue-400">Vue + Tailwind 4.1</h1>
+    <h1 class="text-4xl font-bold mb-6 text-purple-700">Vue + Tailwind 4.1</h1>
     <HelloWorld />
   </div>
 </template>
 
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
