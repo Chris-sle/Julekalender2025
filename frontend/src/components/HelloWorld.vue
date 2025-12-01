691775d71696327f1157922e7de290f1968573c5
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2025-12-01 12:17:22 +0100

    frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-6 bg-gray-800 rounded-xl shadow-lg">
+    <p class="text-lg">🚀 Your Vue project is ready!</p>
+    <p class="text-sm text-gray-400 mt-2">
+      Tailwind CSS v4.1 is active. Edit this component to get started!
+      <p class="text-red-500 text-4xl">Test</p>
+
+    </p>
+  </div>
+</template>
+
+<script setup>
+</script>
