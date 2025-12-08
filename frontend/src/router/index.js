3c986b22eaa7a6e0067213e7203d1aeb4f03c879
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2025-12-08 11:13:47 +0100

    router update login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Calendar from '../views/calendar.vue'
 import AdminPanel from '../views/adminpanel.vue'
+import login from '../views/login.vue'
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/admin',
     name: 'AdminPanel',
     component: AdminPanel
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: login
+  },
 ]
 
 const router = createRouter({
