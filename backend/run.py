6cd46a9c1c5fff0246b99e524a463b1579a2a119
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2025-12-08 15:04:27 +0100

    fix small

--- backend/run.py
+++ backend/run.py
@@ -3,11 +3,12 @@ import os
 from flask import send_from_directory
 
 app = create_app()
-UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route('/uploads/<path:filename>')
-def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+    UPLOAD_FOLDER = os.path.join(os.patch.firname(os.path.abspath(__file__)), 'uploads')
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                     build_only=False,
+                     view_func=lambda filename: send_from_directory(app.config['UPLOAD_FOLDER'], filename))
